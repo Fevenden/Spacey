54cbb23d3c45f8e07c7b5066c4ea9fe29d4775dd
Freedom Evenden <ftevenden1@gmail.com>
2019-12-31 00:33:27 -0900

    created click handler and function to fetch nasa data ,

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function handleSearchClick() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+    const searchTerm = document.getElementById('search').value;
+    getNasaImageData(searchTerm);
+  })
+}
+
+function getNasaImageData(searchTerm) {
+  fetch(`https://images-api.nasa.gov/search?q=${searchTerm}&media_type=image&keywords=space&page=1`)
+  .then(response => response.json())
+  .then(responseJson => console.log(responseJson));
+}
+
+
+
+function init() {
+  handleSearchClick()
+}
+
+init()
