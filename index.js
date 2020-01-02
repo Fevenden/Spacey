78f114b3e08202df53886f5e1a451bd0fb3bf2cf
fte96 <ftevenden1@gmail.com>
2020-01-02 11:24:24 -0900

    started working on styling, made picture slideshow

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const maxQ = 10;
 let searchTerm;
 let youTubeSearch;
+let imgCounter = 0;
 
 function handleSearchClick() {
   $('form').submit(function(e) {
@@ -9,19 +10,19 @@ function handleSearchClick() {
     e.preventDefault();
     searchTerm = document.getElementById('search').value;
     youTubeSearch = 'what is' + searchTerm;
-    apiCall(`https://images-api.nasa.gov/search?q=${searchTerm}&media_type=image&keywords=space&page=1`, displayNasaData);
+    apiCall(`https://images-api.nasa.gov/search?q=${searchTerm}&media_type=image&keywords=space&page=1`).then(response => displayNasaData(response))
   });
 }
 
-function apiCall(url, onComplete) {
-  fetch(url)
+function apiCall(url) {
+  return fetch(url)
   .then(response => { 
     if (response.ok) {
       return response.json()
     } 
     throw new Error(response.statusText);
   })
-  .then(responseJson => onComplete(responseJson))
+  .then(responseJson => responseJson)
   .catch(err =>{
     if (!err.message) {
       $('.js-error').append(`Something went wrong: YouTube Quota limit reached, videos unavailable until 12:00am(PT)`)
@@ -38,17 +39,41 @@ function displayNasaData(json) {
     $('.js-images').empty();
     $('.js-wiki').empty();
     $('.js-videos').empty()
-    apiCall(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&key=AIzaSyBI-NvmjhQbV-B-JX5ayx1Vyt_spkuXhEw&type=video&safeSearch=strict&order=Relevance&videoEmbeddable=true&relevanceLanguage=en&topidId=/m/01k8wb&videoCategoryId=27`, displayYouTubeData);
-    apiCall(`https://en.wikipedia.org/w/api.php?action=query&format=json&redirects=1&titles=${searchTerm}&prop=extracts&exsectionformat=plain&origin=*&indexpageids=1&exintro=true&explaintext=true`, displayWikiData);
+    apiCall(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&key=AIzaSyBI-NvmjhQbV-B-JX5ayx1Vyt_spkuXhEw&type=video&safeSearch=strict&order=Relevance&videoEmbeddable=true&relevanceLanguage=en&topidId=/m/01k8wb&videoCategoryId=27`).then(response => displayYouTubeData(response));
+    apiCall(`https://en.wikipedia.org/w/api.php?action=query&format=json&redirects=1&titles=${searchTerm}&prop=extracts&exsectionformat=plain&origin=*&indexpageids=1&exintro=true&explaintext=true`).then(response => displayWikiData(response));
     for (let i = 0; i < maxQ; i++) {
+      let showClass = (i === 0)? 'show':'hidden';
       $('.js-images').append(`
         <li class="nasaImg">
-          <img src="${json.collection.items[i].links[0].href}" alt="${json.collection.items[i].data[0].description_508}" class="spaceImg${[i]}">
+          <img src="${json.collection.items[i].links[0].href}" alt="${json.collection.items[i].data[0].description_508}" class="spaceImg spaceImg${i} ${showClass}">
         </li>
       `);
-      $(`.spaceImg${[i + 1]}`).toggleClass('hidden')
     };
   }
+}
+
+function handleNextPrevClick() {
+  $('.next').click(function(e) {
+  e.preventDefault();
+  let array = $('.spaceImg').toArray();
+  $(array[imgCounter]).removeClass('show').addClass('hidden');
+  imgCounter ++
+  if (imgCounter === maxQ) {
+    imgCounter = 0;
+  };
+  $(array[imgCounter]).removeClass('hidden').addClass('show');
+  });
+
+  $('.previous').click(function(e) {
+  e.preventDefault();
+  let array = $('.spaceImg').toArray();
+  $(array[imgCounter]).removeClass('show').addClass('hidden');
+  imgCounter -- 
+  if (imgCounter === -1) {
+    imgCounter = 9;
+  };
+  $(array[imgCounter]).removeClass('hidden').addClass('show');
+  })
 }
 
 function displayYouTubeData(json) {
@@ -69,6 +94,7 @@ function displayWikiData(json) {
 
 function init() {
   handleSearchClick()
+  handleNextPrevClick()
 }
 
 init()
